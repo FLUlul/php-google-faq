73eeb22dcdccaa28163f4f3585516f830654e145
FLU <valerio.massj@gmail.com>
2021-12-21 15:43:10 +0100

    update:subquestion-subanswer

--- index.php
+++ index.php
@@ -44,7 +44,13 @@
                 Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.
                 Google LLC, con sede negli Stati Uniti, per il resto del mondo.
                 La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.
-                Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account."
+                Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.",
+                "subquestion" => "Stabilire il paese associato al tuo account",
+                "subanswer" => "Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell'ultimo anno.
+
+                I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno per aggiornare l'associazione del paese.
+                
+                Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza tra il paese in cui lavori e il paese in cui risiedi, l'installazione di una rete privata virtuale (VPN) per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. Contattaci se ritieni che il paese associato al tuo account sia sbagliato."
             ],
             [
                 "question" => "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
@@ -61,13 +67,30 @@
     <div class="container">
         <?php
             foreach ($db as $questionTab) {
-                echo "<h2>" . $questionTab["question"] . "</h2>";
-
-                /* explode on every end of the line */
-                $para = explode(PHP_EOL, $questionTab["answer"]);
-                foreach ($para as $expPara) {
-                    echo "<p>" . $expPara . "</p>";
-                }  
+                "<div class='question-answer-tab'>";
+                
+                    /* print the question */
+                    echo "<h2>" . $questionTab["question"] . "</h2>";
+                    /* explode on every end of the line */
+                    $para = explode(PHP_EOL, $questionTab["answer"]);
+                    foreach ($para as $expPara) {
+                        if (strlen($expPara) > 0) {
+                            echo "<p>" . trim($expPara) . "</p>";
+                        }
+                        var_dump(trim($expPara));
+                    }
+                    
+                    if (strlen($questionTab["subquestion"]) > 0 && strlen($questionTab["subanswer"]) > 0) {
+                        /* print the sub question */
+                        echo "<h3>" . $questionTab["subquestion"] . "</h3>";
+                        /* explode the sub answer */
+                        $subPara = explode(PHP_EOL, $questionTab["subanswer"]);
+                        foreach ($subPara as $expSubPara) {
+                            echo "<p>" . $expSubPara . "</p>";
+                        }
+                    }
+                    
+                "</div>";
             }
         ?>
     </div>
